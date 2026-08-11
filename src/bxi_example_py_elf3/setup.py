8973b57a275f0f02aea982d7861146bf5f4a578f
timtimting <1970678863@qq.com>
2026-08-11 14:49:11 +0800

    Add lightweight motor control framework

--- src/bxi_example_py_elf3/setup.py
+++ src/bxi_example_py_elf3/setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 import os
 
 package_name = 'bxi_example_py_elf3'
@@ -50,11 +50,7 @@ def get_config_files():
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name,
-              f'{package_name}.inference',
-              f'{package_name}.utils',
-              f'{package_name}.control',
-              ],
+    packages=find_packages(exclude=["test", "test.*"]),
     data_files=[
         ('share/ament_index/resource_index/packages',['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
